0d2cd340a17a0dbd056e7178f2e5a602601e9e29
Sherry <sherryyue.c@protonmail.com>
2021-05-25 23:34:03 +0800

    support external link

--- www/js/index.js
+++ www/js/index.js
@@ -3,26 +3,52 @@ var iab;
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    openSite();
+    openTrnt();
 }
 
-function openSite() {
+function openTrnt() {
     let url = "https://trnt.lgbt/forum.php";
     let target = "_blank";
     let options = "hidden=no,clearcache=no,clearsessioncache=no,footer=no,hardwareback=yes,hidenavigationbuttons=yes,hideurlbar=yes,zoom=no,mediaPlaybackRequiresUserAction=no,shouldPauseOnSuspend=yes,useWideViewPort=yes,fullscreen=yes,location=no";
     iab = cordova.InAppBrowser.open(url, target, options);
-    //replace window.open()
-    window.open = cordova.InAppBrowser.open;
+    bindEvent();
 }
 
-function insertScript(){
-    ref.addEventListener('loadstop', function() {
-        //ref.executeScript({file: "myscript.js"});
+function bindEvent() {
+    iab.addEventListener('loadstart', function (event) {
+        console.warn("loadstart", event);
+        openExteralLink(event.url);
+    });
+
+    iab.addEventListener('loadstop', function (event) {
+        console.warn("loadstop", event);
+        openExteralLink(event.url);
+    });
+
+    iab.addEventListener('loaderror', function (event) {
+        console.warn("loaderror", event);
+        openExteralLink(event.url);
     });
 }
 
-function insertStyle(){
-    ref.addEventListener('loadstop', function() {
-        //ref.insertCSS({file: "mystyles.css"});
-    });
+function insertScript() {
+    //ref.executeScript({file: "myscript.js"});
+}
+
+function insertStyle() {
+    //ref.insertCSS({file: "mystyles.css"});
+}
+
+function openExteralLink(url) {
+    if (url.match(/^tel:/) ||
+        url.match(/^sms:/) ||
+        url.match(/^mailto:/) ||
+        url.match(/^geo:/) ||
+        url.match(/^whatsapp:/) ||
+        url.match(/^tg:/) ||
+        url.match(/^line:/)) {
+        let target = "_system";
+        let options = "hidden=no,clearcache=no,clearsessioncache=no,footer=no,hardwareback=yes,hidenavigationbuttons=yes,hideurlbar=yes,zoom=no,mediaPlaybackRequiresUserAction=no,shouldPauseOnSuspend=yes,useWideViewPort=yes,fullscreen=yes,location=no";
+        iab = cordova.InAppBrowser.open(url, target, options);
+    }
 }
